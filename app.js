375945ea105ab62a810af291c534341e8103b892
Matheus <79723385+pitouemat@users.noreply.github.com>
2021-09-04 23:13:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const pokemonFetch = () => {
+
+    const search = document.getElementById('src')
+    const label = document.getElementById('label')
+
+    label.addEventListener("click", () => { 
+        let name = String(search.value)
+        
+        let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+   
+    
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (dados) {
+        
+        let pokemon = dados.forms[0].name
+        let sprite = dados.sprites.front_default
+        const stats = dados.stats
+     
+        const img = document.getElementById('pokeimg')
+        img.src = sprite
+
+        const shiny = document.getElementById('shiny')
+
+        let nameH1 = document.getElementById('name')
+       
+        nameH1.innerHTML = "Name: " +  pokemon
+
+         
+        let number = document.getElementById('number')
+        number.innerHTML = "Number: " + dados.id+ " N°"
+
+        let type = document.getElementById('type')
+        type.innerHTML = "Types: " +  dados.types.map(item => ' ' + item.type.name).toString()
+
+        let weight = document.getElementById("weight")
+        weight.innerHTML = "Weight: " + dados.weight / 10 + " Kg"
+
+        let height = document.getElementById('height')
+        height.innerHTML = "Height: " + dados.height  / 10 + " M"
+
+        shiny.addEventListener("change", ()=> {
+            if(shiny.checked){
+                 sprite =  dados.sprites.front_shiny
+                img.src = sprite
+            }else{
+                sprite = dados.sprites.front_default
+                img.src = sprite
+            }
+           
+        })
+
+        
+       
+    });
+
+    
+})
+
+}
+
+pokemonFetch()
